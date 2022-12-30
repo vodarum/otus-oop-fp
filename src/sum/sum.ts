5f4d01d8de1ea57727214a6632c23d1eb23e23e1
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2022-12-31 01:14:20 +0300

    Add "sum"

---
+++ src/sum/sum.ts
@@ -0,0 +1,13 @@
+export default function sum(value?: number) {
+  let s = value ?? 0;
+
+  function foo(newValue: number) {
+    s += newValue;
+
+    return sum(s);
+  };
+
+  foo.valueOf = () => s;
+
+  return foo;
+}
